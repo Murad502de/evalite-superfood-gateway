26fb795b2afe97a436a833812c498e94b49d0488
itWelt <rassulovmurad@gmail.com>
2023-04-16 00:58:19 +0300

    [main][test]

--- application/routes/api.php
+++ application/routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use Illuminate\Http\Request;
+// use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,8 +12,10 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider and all of them will
 | be assigned to the "api" middleware group. Make something great!
 |
-*/
+ */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::prefix('v1')->group(function () {
+    Route::get('test', function () {
+        return 'evalite api v1 test response';
+    });
 });
