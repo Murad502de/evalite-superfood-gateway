8037bdf9b4588630c5f5bbd5c14a813fd9a5369b
itWelt <rassulovmurad@gmail.com>
2023-04-19 16:29:40 +0300

    [main][task] edit routes/api

--- application/routes/api.php
+++ application/routes/api.php
@@ -15,7 +15,4 @@ use Illuminate\Support\Facades\Route;
  */
 
 Route::prefix('v1')->group(function () {
-    Route::get('test', function () {
-        return 'evalite api v1 test response';
-    });
 });
