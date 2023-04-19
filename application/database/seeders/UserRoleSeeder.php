c7a3f7ec0802beb2de8ccb8e0b7d29ee1fa52aed
itWelt <rassulovmurad@gmail.com>
2023-04-20 02:36:43 +0300

    [main][task] created App\Traits\SharedUserRolesTrait

--- application/database/seeders/UserRoleSeeder.php
+++ application/database/seeders/UserRoleSeeder.php
@@ -3,17 +3,20 @@
 namespace Database\Seeders;
 
 use App\Models\UserRole;
+use App\Traits\SharedUserRolesTrait;
 use Illuminate\Database\Seeder;
 
 class UserRoleSeeder extends Seeder
 {
+    use SharedUserRolesTrait;
+
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
         UserRole::create([
-            'code'       => 'admin',
+            'code'       => self::$USER_ROLE_ADMIN,
             'name'       => 'администратор',
             'is_default' => true,
         ]);
--- application/database/seeders/UserSeeder.php
+++ application/database/seeders/UserSeeder.php
@@ -3,28 +3,32 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-use Illuminate\Database\Seeder;
+use App\Models\UserRole;
+use App\Traits\SharedGendersTrait;
 use Carbon\Carbon;
+use Illuminate\Database\Seeder;
+use App\Traits\SharedUserRolesTrait;
 
 class UserSeeder extends Seeder
 {
+    use SharedGendersTrait, SharedUserRolesTrait;
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
         User::create([
-            'user_role_id' => '',
-            'first_name' => 'adminFirstName',
-            'second_name' => 'adminSecondName',
-            'third_name' => 'adminThirdName',
-            'gender' => 'male',
-            'birthday' => Carbon::now(),
-            'email' => '',
-            'password' => '',
-            'token' => '',
-            'phone' => '',
-            'invite_code' => '',
+            'user_role_id'    => UserRole::whereCode(self::$USER_ROLE_ADMIN),
+            'first_name'      => 'adminFirstName',
+            'second_name'     => 'adminSecondName',
+            'third_name'      => 'adminThirdName',
+            'gender'          => self::$GENDER_MALE,
+            'birthday'        => Carbon::now(),
+            'email'           => '',
+            'password'        => '',
+            'token'           => '',
+            'phone'           => '',
+            'invite_code'     => '',
             'individual_code' => '',
         ]);
     }
