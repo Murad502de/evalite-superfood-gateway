6decf4b74f36d3c550f54e4a443ffcfb90f0aba0
itWelt <rassulovmurad@gmail.com>
2023-04-19 15:53:28 +0300

    [main][task] created UserRole -m

--- application/database/migrations/2023_04_19_124528_create_user_roles_table.php
+++ application/database/migrations/2023_04_19_124528_create_user_roles_table.php
@@ -11,14 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+        Schema::create('user_roles', function (Blueprint $table) {
+            $table->id()->index();
             $table->timestamps();
+            $table->string('uuid')->index();
+            $table->string('code')->index();
+            $table->string('name');
         });
     }
 
@@ -27,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('user_roles');
     }
 };
