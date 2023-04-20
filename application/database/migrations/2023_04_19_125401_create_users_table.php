dad3d4e1991dff8a6f60d72eb01dbe8352d06ec6
itWelt <rassulovmurad@gmail.com>
2023-04-21 00:11:47 +0300

    [main][dev] added employment_type for User -m

--- application/database/migrations/2023_04_19_125401_create_users_table.php
+++ application/database/migrations/2023_04_19_125401_create_users_table.php
@@ -1,11 +1,16 @@
 <?php
 
+use App\Traits\SharedEmploymentTypesTrait;
+use App\Traits\SharedGendersTrait;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
+    use SharedGendersTrait,
+        SharedEmploymentTypesTrait;
+
     /**
      * Run the migrations.
      */
@@ -19,8 +24,9 @@ return new class extends Migration
             $table->string('first_name');
             $table->string('second_name');
             $table->string('third_name');
-            $table->enum('gender', ['male', 'female']);
+            $table->enum('gender', [self::$GENDER_MALE, self::$GENDER_FEMALE]);
             $table->date('birthday');
+            $table->enum('employment_type', [self::$INDIVIDUAL_ENTREPRENEUR, self::$SELF_EMPLOYED]);
             $table->string('email')->index();
             $table->string('password');
             $table->string('token');
--- application/database/seeders/UserSeeder.php
+++ application/database/seeders/UserSeeder.php
@@ -2,10 +2,11 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
 use App\Models\Role;
+use App\Models\User;
 use App\Traits\GenerateUserTokenTrait;
 use App\Traits\PasswordEncryptTrait;
+use App\Traits\SharedEmploymentTypesTrait;
 use App\Traits\SharedGendersTrait;
 use App\Traits\SharedRolesTrait;
 use Carbon\Carbon;
@@ -13,19 +14,24 @@ use Illuminate\Database\Seeder;
 
 class UserSeeder extends Seeder
 {
-    use SharedGendersTrait, SharedRolesTrait, PasswordEncryptTrait, GenerateUserTokenTrait;
+    use SharedGendersTrait,
+    SharedRolesTrait,
+    PasswordEncryptTrait,
+    GenerateUserTokenTrait,
+        SharedEmploymentTypesTrait;
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
         User::create([
-            'role_id'    => Role::whereCode(self::$USER_ROLE_ADMIN)->first()->id,
+            'role_id'         => Role::whereCode(self::$USER_ROLE_ADMIN)->first()->id,
             'first_name'      => 'adminFirstName',
             'second_name'     => 'adminSecondName',
             'third_name'      => 'adminThirdName',
             'gender'          => self::$GENDER_MALE,
             'birthday'        => Carbon::now(),
+            'employment_type' => self::$INDIVIDUAL_ENTREPRENEUR,
             'email'           => config('app.admin_email'),
             'password'        => self::passwordEncrypt(config('app.admin_password')),
             'token'           => self::generateUserToken(),
