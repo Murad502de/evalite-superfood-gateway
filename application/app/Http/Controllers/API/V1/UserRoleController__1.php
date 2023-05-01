0755e641494ae34126b815bc886d548db8341035
itWelt <rassulovmurad@gmail.com>
2023-05-01 14:54:27 +0300

    [main][task] intalled dompdf

--- application/app/Http/Controllers/API/V1/UserRoleController__1.php
+++ application/app/Http/Controllers/API/V1/UserRoleController__1.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\API\V1;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class RoleController__1 extends Controller
+class UserRoleController__1 extends Controller
 {
     public function create(Request $request)
     {}
