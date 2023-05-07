a14b1b9b7255e4f290cdd8c2fc098d73c9025e2a
itWelt <rassulovmurad@gmail.com>
2023-05-07 22:29:30 +0300

    [main][task] implemented auth node for amocrm service

--- application/app/Http/Controllers/API/V1/ServicesAmoCrmController__1.php
+++ application/app/Http/Controllers/API/V1/ServicesAmoCrmController__1.php
@@ -5,41 +5,47 @@ namespace App\Http\Controllers\API\V1;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\API\V1\ServicesAmoCrmAuthRequest__1;
 use App\Models\Amocrm;
+use App\Models\Configuration;
 use App\Services\amoAPI\amoAPIHub;
+use Illuminate\Http\Response;
 
 class ServicesAmoCrmController__1 extends Controller
 {
     public function signin(ServicesAmoCrmAuthRequest__1 $request)
     {
-        $authData = [
-            'client_id'     => $request->all()['client_id'],
-            'client_secret' => config('services.amoCRM.client_secret'), // TODO get from Configuration model
-            'code'          => $request->all()['code'],
-            'redirect_uri'  => config('services.amoCRM.redirect_uri'), // TODO get from Configuration model
-            'subdomain'     => config('services.amoCRM.subdomain'), // TODO get from Configuration model
-        ];
-        $amo         = new amoAPIHub($authData);
-        $response    = $amo->auth();
-        $accountData = [
-            'client_id'     => $request->all()['client_id'],
-            'client_secret' => config('services.amoCRM.client_secret'), // TODO get from Configuration model
-            'subdomain'     => $authData['subdomain'],
-            'access_token'  => $response['access_token'],
-            'redirect_uri'  => $authData['redirect_uri'],
-            'token_type'    => $response['token_type'],
-            'refresh_token' => $response['refresh_token'],
-            'when_expires'  => time() + (int) $response['expires_in'] - 400,
-        ];
+        $configuration = Configuration::first();
 
-        Amocrm::signin($accountData);
-        return response(['OK'], 200);
+        if ($configuration) {
+            $amo = new amoAPIHub([
+                'client_id'     => $request->all()['client_id'],
+                'client_secret' => $configuration->amocrm_client_secret,
+                'code'          => $request->all()['code'],
+                'redirect_uri'  => $configuration->amocrm_redirect_uri,
+                'subdomain'     => $configuration->amocrm_subdomain,
+            ]);
+            $response    = $amo->auth();
+            $accountData = [
+                'client_id'     => $request->all()['client_id'],
+                'client_secret' => $configuration->amocrm_client_secret,
+                'subdomain'     => $configuration->amocrm_subdomain,
+                'access_token'  => $response['access_token'],
+                'redirect_uri'  => $configuration->amocrm_redirect_uri,
+                'token_type'    => $response['token_type'],
+                'refresh_token' => $response['refresh_token'],
+                'when_expires'  => time() + (int) $response['expires_in'] - 400,
+            ];
+            Amocrm::signin($accountData);
+        }
+
+        return response()->json(['message' => 'success'], Response::HTTP_OK);
     }
     public function signout()
     {
         Amocrm::signout();
-        return response(['OK'], 200);
+        return response()->json(['message' => 'success'], Response::HTTP_OK);
     }
-    public function webhookLead() {
-        return response(['OK'], 200);
+    public function webhookLead()
+    {
+        return response()->json(['message' => 'success'], Response::HTTP_OK);
     }
 }
