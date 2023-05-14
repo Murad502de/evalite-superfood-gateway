1fe80bc59960a185885fa6d106fc7cf4e6e38498
itWelt <rassulovmurad@gmail.com>
2023-05-14 17:53:05 +0300

    [main][task] implemented status filter for getting of pays for admin

--- application/app/Http/Controllers/API/V1/PayoutController__1.php
+++ application/app/Http/Controllers/API/V1/PayoutController__1.php
@@ -5,13 +5,20 @@ namespace App\Http\Controllers\API\V1;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\API\V1\PayoutsResource;
 use App\Models\Payout;
+use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 
 class PayoutController__1 extends Controller
 {
-    public function getPayouts()
+    public function getPayouts(Request $request)
     {
-        return PayoutsResource::collection(Payout::all());
+        $requestParams = $request->all();
+        $filterStatus  = isset($requestParams['filters']) && isset($requestParams['filters']['status']) ? $requestParams['filters']['status'] : null;
+        $payouts       = Payout::when($filterStatus, function ($query) use ($filterStatus) {
+            $query->whereStatus($filterStatus);
+        })->get();
+
+        return PayoutsResource::collection($payouts);
     }
     public function getPayout(Payout $payout)
     {
