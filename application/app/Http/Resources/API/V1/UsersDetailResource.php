0f7609cfa886304df2fea9a734cf09b220e4bd39
itWelt <rassulovmurad@gmail.com>
2023-05-20 20:02:43 +0300

    [main][task] implemented UsersDetailResource

--- application/app/Http/Resources/API/V1/UsersDetailResource.php
+++ application/app/Http/Resources/API/V1/UsersDetailResource.php
@@ -5,6 +5,7 @@ namespace App\Http\Resources\API\V1;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Arr;
 
 class UsersDetailResource extends JsonResource
 {
@@ -15,18 +16,19 @@ class UsersDetailResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
+        $avatar                               = $this->getMedia(User::MEDIA_PREFIX_AVATAR . $this->uuid)->first();
         $passport                             = $this->passport ? new UsersPassportResource__1($this->passport) : null;
         $paymentDetailsIndividualEntrepreneur = $this->paymentDetailsIndividualEntrepreneur ? new UsersPaymentDetailsIndividualEntrepreneurResource__1($this->paymentDetailsIndividualEntrepreneur) : null;
         $paymentDetailsSelfEmployed           = $this->paymentDetailsSelfEmployed ? new UsersPaymentDetailsSelfEmployedResource__1($this->paymentDetailsSelfEmployed) : null;
         $agencyContract                       = $this->agencyContract ? new UsersAgencyContractResource__1($this->agencyContract) : null;
 
-        return array_merge(parent::toArray($request), [
-            'avatar'                                  => $this->getMedia(User::MEDIA_PREFIX_AVATAR . $this->uuid)->first()->getUrl(),
+        return Arr::except(array_merge(parent::toArray($request), [
+            'avatar'                                  => $avatar ? $avatar->getUrl() : null,
             'role'                                    => $this->role->code,
             'passport'                                => $passport,
             'payment_details_individual_entrepreneur' => $paymentDetailsIndividualEntrepreneur,
             'payment_details_self_employed'           => $paymentDetailsSelfEmployed,
             'agency_contract'                         => $agencyContract,
-        ]);
+        ]), ['media']);
     }
 }
