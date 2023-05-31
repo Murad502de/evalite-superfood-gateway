2295172f6be360a20b16fae23efaa024059a8e47
itWelt <rassulovmurad@gmail.com>
2023-05-31 20:11:38 +0300

    [main][task] implemented UserApprovedEvent

--- application/app/Events/UserApprovedEvent.php
+++ application/app/Events/UserApprovedEvent.php
@@ -4,11 +4,13 @@ namespace App\Events;
 
 use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
 use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-class UserRegisteredEvent
+class UserApprovedEvent
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
