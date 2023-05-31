d43e14f84801d127bf57b534aa7707447c1357e5
itWelt <rassulovmurad@gmail.com>
2023-05-31 20:55:39 +0300

    [main][task] implemented UserApprovedMail

--- application/app/Listeners/UserApprovedListener.php
+++ application/app/Listeners/UserApprovedListener.php
@@ -4,7 +4,8 @@ namespace App\Listeners;
 
 use App\Events\UserApprovedEvent;
 use Illuminate\Contracts\Queue\ShouldQueue;
-use Illuminate\Support\Facades\Log; //DELETE
+use Illuminate\Support\Facades\Mail;
+use App\Mail\UserApprovedMail;
 
 class UserApprovedListener implements ShouldQueue
 {
@@ -12,15 +13,13 @@ class UserApprovedListener implements ShouldQueue
      * Create the event listener.
      */
     public function __construct()
-    {
-        Log::info(__METHOD__); //DELETE
-    }
+    {}
 
     /**
      * Handle the event.
      */
     public function handle(UserApprovedEvent $event): void
     {
-        Log::info(__METHOD__, [$event->user->email]); //DELETE
+        Mail::to($event->user->email)->send(new UserApprovedMail($event->user));
     }
 }
