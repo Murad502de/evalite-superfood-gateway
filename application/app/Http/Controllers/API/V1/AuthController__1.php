845a280a567ef571077363b0d3cad363a5649cd7
itWelt <rassulovmurad@gmail.com>
2023-10-19 21:35:50 +0300

    [main][task] EV-6 implement SignupRequest__1

--- application/app/Http/Controllers/API/V1/AuthController__1.php
+++ application/app/Http/Controllers/API/V1/AuthController__1.php
@@ -3,11 +3,12 @@
 namespace App\Http\Controllers\API\V1;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Http\Requests\API\V1\SignupRequest__1;
 
 class AuthController__1 extends Controller
 {
-    public function signup(Request $request) {
+    public function signup(SignupRequest__1 $request)
+    {
         return 'AuthController__1/signup';
     }
 }
