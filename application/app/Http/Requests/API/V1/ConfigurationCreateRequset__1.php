8fa3862a90c12d14ac6a8ba917fe123fef7b2dd2
itWelt <rassulovmurad@gmail.com>
2023-11-05 15:27:36 +0300

    [main][] edit ConfigurationCreateRequset__1

--- application/app/Http/Requests/API/V1/ConfigurationCreateRequset__1.php
+++ application/app/Http/Requests/API/V1/ConfigurationCreateRequset__1.php
@@ -2,24 +2,17 @@
 
 namespace App\Http\Requests\API\V1;
 
+use App\Models\Role;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Support\Facades\Config;
 
 class ConfigurationCreateRequset__1 extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
     public function authorize(): bool
     {
-        return Config::get('user')->role->code === config('constants.user.roles.admin');
+        return Config::get('user')->role->code === Role::$ROLE_CODE_ADMIN;
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\Rule|array|string>
-     */
     public function rules(): array
     {
         return [
