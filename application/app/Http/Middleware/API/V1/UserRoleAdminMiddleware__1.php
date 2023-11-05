f427d1583763eba0f002d0dd62d040eff3fdc8bc
itWelt <rassulovmurad@gmail.com>
2023-11-05 15:36:27 +0300

    [main][] add user role code to UserRoleAdminMiddleware__1

--- application/app/Http/Middleware/API/V1/UserRoleAdminMiddleware__1.php
+++ application/app/Http/Middleware/API/V1/UserRoleAdminMiddleware__1.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware\API\V1;
 
 use App\Exceptions\ForbiddenException;
+use App\Models\Role;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Config;
@@ -10,14 +11,9 @@ use Symfony\Component\HttpFoundation\Response;
 
 class UserRoleAdminMiddleware__1
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
-     */
     public function handle(Request $request, Closure $next): Response
     {
-        if (Config::get('user')->role->code === config('constants.user.roles.admin')) {
+        if (Config::get('user')->role->code === Role::$ROLE_CODE_ADMIN) {
             return $next($request);
         }
 
