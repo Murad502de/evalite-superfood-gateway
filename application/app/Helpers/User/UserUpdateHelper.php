0479fdffb6b7edb38c31a67786d6d785eb95ed87
itWelt <rassulovmurad@gmail.com>
2023-11-12 15:59:30 +0300

    [main][] implement UserUpdateHelper prototype

---
+++ application/app/Helpers/User/UserUpdateHelper.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Helpers\User;
+
+use Illuminate\Support\Facades\Log;
+
+class UserUpdateHelper
+{
+    public function update()
+    {
+        Log::info(__METHOD__); //DELETE
+    }
+}
