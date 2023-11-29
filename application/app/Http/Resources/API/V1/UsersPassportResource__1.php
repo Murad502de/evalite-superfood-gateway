26976aafef0bd3c0612cac6e3e8c002cc01a09eb
itWelt <rassulovmurad@gmail.com>
2023-11-30 01:00:22 +0300

    [main][] fix UsersPassportResource__1

--- application/app/Http/Resources/API/V1/UsersPassportResource__1.php
+++ application/app/Http/Resources/API/V1/UsersPassportResource__1.php
@@ -8,17 +8,16 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 class UsersPassportResource__1 extends JsonResource
 {
-    /**
-     * Transform the resource into an array.
-     *
-     * @return array<string, mixed>
-     */
     public function toArray(Request $request): array
     {
+        $passport_main_spread         = $this->getMedia(Passport::MEDIA_PREFIX_MAIN_SPREAD . $this->uuid)->first();
+        $passport_registration_spread = $this->getMedia(Passport::MEDIA_PREFIX_REGISTRATION_SPREAD . $this->uuid)->first();
+        $passport_verification_spread = $this->getMedia(Passport::MEDIA_PREFIX_VERIFICATION_SPREAD . $this->uuid)->first();
+
         return array_merge(parent::toArray($request), [
-            'passport_main_spread'         => $this->getMedia(Passport::MEDIA_PREFIX_MAIN_SPREAD . $this->uuid)->first()->getUrl(),
-            'passport_registration_spread' => $this->getMedia(Passport::MEDIA_PREFIX_REGISTRATION_SPREAD . $this->uuid)->first()->getUrl(),
-            'passport_verification_spread' => $this->getMedia(Passport::MEDIA_PREFIX_VERIFICATION_SPREAD . $this->uuid)->first()->getUrl(),
+            'passport_main_spread'         => $passport_main_spread ? $passport_main_spread->getUrl() : null,
+            'passport_registration_spread' => $passport_registration_spread ? $passport_registration_spread->getUrl() : null,
+            'passport_verification_spread' => $passport_verification_spread ? $passport_verification_spread->getUrl() : null,
         ]);
     }
 }
