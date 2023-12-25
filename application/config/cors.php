2208264332af50228fd6dfa2ed63d2f3107a9d26
itWelt <rassulovmurad@gmail.com>
2023-12-25 14:10:28 +0300

    [dev][]

--- application/config/cors.php
+++ application/config/cors.php
@@ -19,9 +19,9 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'], //TODO dev
+    // 'allowed_origins' => ['*'], //TODO dev
 
-    // 'allowed_origins' => [],
+    'allowed_origins' => [],
 
     'allowed_origins_patterns' => [],
 
