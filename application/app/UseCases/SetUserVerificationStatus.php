e741e7d6415700ccf84b4de8678eaf629d9e927c
itWelt <rassulovmurad@gmail.com>
2024-01-12 21:47:15 +0300

    [main][]

--- application/app/UseCases/SetUserVerificationStatus.php
+++ application/app/UseCases/SetUserVerificationStatus.php
@@ -2,6 +2,7 @@
 
 namespace App\UseCases;
 
+use App\Interactors\SendStatusNotification;
 use App\Interactors\UpdateUserVerificationStatus;
 use App\Models\User;
 use Illuminate\Http\Request;
@@ -11,14 +12,21 @@ class SetUserVerificationStatus
     public function __invoke(User $user, Request $request): bool
     {
         dump(__METHOD__); //DELETE
+        $STATUS = $request->user_verification_status;
         //TODO set status
-        $updateUserVerificationStatusRes = (new UpdateUserVerificationStatus)($user, $request->user_verification_status);
+        $updateUserVerificationStatusRes = (new UpdateUserVerificationStatus)($user, $STATUS);
 
         if (!$updateUserVerificationStatusRes) {
             return false;
         }
 
         //TODO send notification by status
+        $sendStatusNotificationRes = (new SendStatusNotification)($user, $STATUS);
+
+        if (!$sendStatusNotificationRes) {
+            return false;
+        }
+
         return true;
     }
 }
