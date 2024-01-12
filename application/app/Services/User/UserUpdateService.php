013fc361960c9619ed192259b91c3cbe19440b92
itWelt <rassulovmurad@gmail.com>
2024-01-12 20:57:39 +0300

    [main][] implemented update in UserUpdateService

--- application/app/Services/User/UserUpdateService.php
+++ application/app/Services/User/UserUpdateService.php
@@ -4,6 +4,7 @@ namespace App\Services\User;
 
 use App\Helpers\User\UserUpdateHelper;
 use App\Models\User;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class UserUpdateService
@@ -29,5 +30,18 @@ class UserUpdateService
         dump(__METHOD__); //DELETE
         dump($user); //DELETE
         dump($data); //DELETE
+
+        $user->update([
+            'first_name'          => isset($data['first_name']) ? $data['first_name'] : $user->first_name,
+            'second_name'         => isset($data['second_name']) ? $data['second_name'] : $user->second_name,
+            'third_name'          => isset($data['third_name']) ? $data['third_name'] : $user->third_name,
+            'gender'              => isset($data['gender']) ? $data['gender'] : $user->gender,
+            'birthday'            => isset($data['birthday']) ? Carbon::parse($data['birthday']) : $user->birthday,
+            'employment_type'     => isset($data['employment_type']) ? $data['employment_type'] : $user->employment_type,
+            'email'               => isset($data['email']) ? $data['email'] : $user->email,
+            'phone'               => isset($data['phone']) ? $data['phone'] : $user->phone,
+            'password'            => isset($data['password']) ? User::passwordEncrypt($data['password']) : $user->password,
+            'verification_status' => isset($data['verification_status']) ? $data['verification_status'] : $user->verification_status,
+        ]);
     }
 }
