20a9c4ea0ba8068e7679115e41b7d8af3a1651a3
itWelt <rassulovmurad@gmail.com>
2024-01-12 18:39:54 +0300

    [main][] add calling UpdateUserVerificationStatus to SetUserVerificationStatus

--- application/app/UseCases/SetUserVerificationStatus.php
+++ application/app/UseCases/SetUserVerificationStatus.php
@@ -8,11 +8,13 @@ use Illuminate\Http\Request;
 
 class SetUserVerificationStatus
 {
-    public function __invoke(User $user, Request $request): void
+    public function __invoke(User $user, Request $request): bool
     {
         dump(__METHOD__); //DELETE
         //TODO set status
         $updateUserVerificationStatusRes = (new UpdateUserVerificationStatus)($user, $request->user_verification_status);
+
         //TODO send notification
+        if (!$updateUserVerificationStatusRes) return false;
     }
 }
